f950ed0e1278c11cfcca4df98aa8e06f5b88405a
Stefano Romanelli <stefanoromanelli86@gmail.com>
2024-02-20 19:16:08 +0100

    add js and contact form

---
+++ javascript/form-validation.js
@@ -0,0 +1,28 @@
+// const email = document.getElementById("email");
+
+// email.addEventListener("input", (event) => {
+//   if (email.validity.typeMismatch) {
+//     email.setCustomValidity("I am expecting an email address!");
+//   } else {
+//     email.setCustomValidity("");
+//   }
+// });
+
+// function validateFields() {
+//   validateName();
+//   validateEmail()
+// }
+
+function validateName() {
+
+ const name = document.getElementById('full-name').value;
+
+ if (name == "") {
+  email.setCustomValidity("Holi")
+ } return false;
+
+}
+ 
+//  console.log(email);
+
+// }
