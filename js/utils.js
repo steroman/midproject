3613a460d4cac79cc747b3023f2fb40f7a9cc8c2
Stefano Romanelli <stefanoromanelli86@gmail.com>
2024-02-24 10:07:40 +0100

    fix bug button not working

--- js/utils.js
+++ js/utils.js
@@ -2,6 +2,6 @@ const getInTouchbutton = document.querySelectorAll(".contact-button");
 
 getInTouchbutton.forEach((button) => {
   button.addEventListener("click", function () {
-    window.location.href = "${window.location.origin}/pages/contact.html";
+    window.location.href = `${window.location.origin}/pages/contact.html`;
   });
 });
