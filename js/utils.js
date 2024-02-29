d9bbd6790a4c7232362dafe63b42931ef55800a6
Stefano Romanelli <stefanoromanelli86@gmail.com>
2024-02-29 22:47:22 +0100

    function to fix scrolling to sections

--- js/utils.js
+++ js/utils.js
@@ -6,6 +6,7 @@ getInTouchbutton.forEach((button) => {
   });
 });
 
+/* FUNCTION TO TOGGLE THE MOBILE MENU */
 
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
@@ -19,3 +20,19 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
   hamburger.classList.remove("active");
   navMenu.classList.remove("active");
 }))
+
+/* FUNCTION TO MAKE THE SECTION HEADINGS VISIBLE */
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      let target = document.querySelector(this.getAttribute('href'));
+      if (target) {
+          window.scrollTo({
+              top: target.offsetTop - 150, // Adjust the offset according to the height of your fixed navigation bar
+              behavior: 'smooth'
+          });
+      }
+  });
+});
