f5c62d7d7d385dac8656ba56504f76e7969b9c0f
Stefano Romanelli <stefanoromanelli86@gmail.com>
2024-03-01 08:09:17 +0100

    review comments in utils

--- js/utils.js
+++ js/utils.js
@@ -34,7 +34,7 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
     let target = document.querySelector(this.getAttribute("href"));
     if (target) {
       window.scrollTo({
-        top: target.offsetTop - 150, // Adjust the offset according to the height of your fixed navigation bar
+        top: target.offsetTop - 150, // ADJUST THE OFFSET OF THE SECTION
         behavior: "smooth",
       });
     }
